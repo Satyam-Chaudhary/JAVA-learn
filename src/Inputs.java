2b46070429109e9e6c2ac4f2828ff90ec06d2915
Satyam Chaudhary <chaudharysatyam313@Satyams-MacBook-Air.local>
2022-11-26 18:32:11 +0530

    java 2nd commit

---
+++ src/Inputs.java
@@ -0,0 +1,5 @@
+public class Inputs {
+    public static void main(String[] args) {
+        System.out.println("hey");
+    }
+}
---
+++ src/primitives.java
@@ -0,0 +1,21 @@
+public class primitives {
+    //any datatype which can't be brpken furtther is primitive eg- int,character
+    public static void main(String[] args) {
+        int rollno = 69;
+        char startname = 's';
+        float marks = 98.668f;
+        double largeDecimalNumbers = 1234567.765442;
+        long largeInteger = 993477893949L;
+        boolean check = true;//or false
+        //String is not primitive
+        String name = "Satyam Chaudhary";
+        System.out.println(rollno);
+        System.out.println(startname);
+        System.out.println(marks);
+        System.out.println(largeDecimalNumbers);
+        System.out.println(largeInteger);
+        System.out.println(check);
+        System.out.println(name);
+
+    }
+}
