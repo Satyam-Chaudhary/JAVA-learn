c3e5b3579d9519ceedd51a51847c30162dce458b
Satyam Chaudhary <chaudharysatyam313@Satyams-MacBook-Air.local>
2022-11-27 12:47:20 +0530

    sum program

--- src/Inputs.java
+++ src/Inputs.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Inputs {
     public static void main(String[] args) {
-        System.out.println("hey");
+        System.out.print("please enter some input:");
+        Scanner input = new Scanner(System.in);
+//        int rollno = input.nextInt();
+//        System.out.println("Your roll number is:" +rollno);
+
+//        String name = input.nextLine();
+ //       System.out.println(name);
+
+//        float marks = input.nextFloat();
+//        System.out.println(marks);
+
+
     }
 }
---
+++ src/sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class sum
+{
+    public static void main(String[] args) {
+//        System.out.println("enter a value:");
+//        Scanner input = new Scanner(System.in);
+//        int a = input.nextInt();
+//        System.out.println("enter b value:");
+//        Scanner inputb = new Scanner(System.in);
+//        int b = inputb.nextInt();
+//        int sum;
+//        sum = a + b;
+//        System.out.println(sum);
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.print("Sum is:"+ sum);
+
+    }
+}
