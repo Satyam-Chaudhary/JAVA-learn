13ecfa972885a6cc202d187f9af550bec8717cc3
Satyam Chaudhary <chaudharysatyam313@Satyams-MacBook-Air.local>
2022-11-29 19:41:13 +0530

    typecasting completed

--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -12,7 +12,7 @@ public class Typecasting {
 
         //integer value can be converted to float, but float value won't convert into integer as float is bigger than integer.
 
-        // TYPE CASTING explictly telling to print floatt value in integer type.
+        // TYPE CASTING - explictly telling to print float value in integer type.
 
 //       float num = input.nextFloat();
 //       int i = (int)num;
@@ -23,16 +23,27 @@ public class Typecasting {
 //        System.out.println(num);
 
 
+
+
         // Automatic type conversion in expressions
-        int a = 257;
-        byte b = (byte)(a);
-        // max value store in byte is 256, therefore it prints the difference of 257 and 256 i.e is 1.
-        System.out.println(b);
+//        int a = 257;
+//        byte b = (byte)(a);
+//        // max value store in byte is 256, therefore it prints the difference of 257 and 256 i.e is 1.
+//        System.out.println(b);
+//
+//        int num ='A';
+//        System.out.println(num);
+        //prints the ASCII valus of A after automatic type conversion.
 
-        int 
-
-
-
-
+//   Automatic type conversion.
+        byte b = 43;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.675f;
+        double d = 0.1234;
+        double result = (f*b) + (i/c) - (d*s);
+        System.out.println((f*b) + "  " + (i/c) + "  " + (d*s));
+        System.out.println(result);
     }
 }
